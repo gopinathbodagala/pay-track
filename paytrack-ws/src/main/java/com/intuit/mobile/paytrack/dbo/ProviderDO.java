0a00a1651ebff27553864bc380c998bd9ace3c9e
vijayan.srinivasan@gmail.com <vijayan.srinivasan@gmail.com@e6bd0a06-6a4f-6c25-1cdf-f6960705fdf0>
2012-12-05 07:51:06 +0000

    Initial import.

---
+++ paytrack-ws/src/main/java/com/intuit/mobile/paytrack/dbo/ProviderDO.java
@@ -0,0 +1,87 @@
+package com.intuit.mobile.paytrack.dbo;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+
+/**
+ * The persistent class for the providers database table.
+ * 
+ */
+public class ProviderDO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+
+	private String address;
+
+	private String emailId;
+
+	private String firstName;
+
+	private String lastName;
+
+	private BigInteger mobileNumber;
+
+	private String serviceName;
+
+	public ProviderDO() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmailId() {
+		return this.emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public BigInteger getMobileNumber() {
+		return this.mobileNumber;
+	}
+
+	public void setMobileNumber(BigInteger mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getServiceName() {
+		return this.serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+}
---
+++ paytrack-ws/src/main/java/com/intuit/mobile/paytrack/dbo/RecieptDO.java
@@ -0,0 +1,78 @@
+package com.intuit.mobile.paytrack.dbo;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.math.BigDecimal;
+
+
+/**
+ * The persistent class for the reciepts database table.
+ * 
+ */
+public class RecieptDO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+
+	private BigDecimal amount;
+
+	private Timestamp from;
+
+	private Timestamp paymentDate;
+
+	private int providerClientsId;
+
+	private Timestamp to;
+
+	public RecieptDO() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public BigDecimal getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public Timestamp getFrom() {
+		return this.from;
+	}
+
+	public void setFrom(Timestamp from) {
+		this.from = from;
+	}
+
+	public Timestamp getPaymentDate() {
+		return this.paymentDate;
+	}
+
+	public void setPaymentDate(Timestamp paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public int getProviderClientsId() {
+		return this.providerClientsId;
+	}
+
+	public void setProviderClientsId(int providerClientsId) {
+		this.providerClientsId = providerClientsId;
+	}
+
+	public Timestamp getTo() {
+		return this.to;
+	}
+
+	public void setTo(Timestamp to) {
+		this.to = to;
+	}
+
+}
