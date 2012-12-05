5b85eeaa85f72626e0c30f260d92f3a00ffe55d3
vijayan.srinivasan@gmail.com <vijayan.srinivasan@gmail.com@e6bd0a06-6a4f-6c25-1cdf-f6960705fdf0>
2012-12-05 08:50:07 +0000

    Initial import.

--- paytrack-ws/src/main/java/com/intuit/mobile/paytrack/dao/ClientDAO.java
+++ paytrack-ws/src/main/java/com/intuit/mobile/paytrack/dao/ClientDAO.java
@@ -1,8 +1,14 @@
 package com.intuit.mobile.paytrack.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.intuit.mobile.paytrack.dbo.ClientDO;
@@ -20,22 +26,45 @@ public class ClientDAO extends BaseDAO {
 
 		Clients clients = new Clients();
 		List<Client> clientList = clients.getClients();
-		
-		for(ClientDO clientDO: clientDOs){
+
+		for (ClientDO clientDO : clientDOs) {
 			Client client = mapper.map(clientDO, Client.class);
 			clientList.add(client);
 		}
 		return clients;
 	}
 
-	public Client save(Long providerId, Client client) {
-		// TODO Auto-generated method stub
-		return null;
+	public Client save(Long providerId, final Client client) {
+		PreparedStatementCreator psc = new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con)
+					throws SQLException {
+				String sql = "INSERT INTO clients (first_name, last_name, "
+						+ " email_id, mobile_number, address) "
+						+ " VALUES (?,?,?,?,?)";
+				PreparedStatement ps = con.prepareStatement(sql,
+						new String[] { "id" });
+				ps.setString(1, client.getFirstName());
+				ps.setString(2, client.getLastName());
+				ps.setString(3, client.getEmail());
+				ps.setString(4, client.getMobile());
+				ps.setString(4, client.getAddress());
+				return ps;
+			}
+
+		};
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(psc, keyHolder);
+		client.setId(keyHolder.getKey().longValue());
+		return client;
 	}
 
-	public Client update(Long providerId, Client client) {
-		// TODO Auto-generated method stub
-		return null;
+	public Client update(Client client) {
+		String sql = "update clients set first_name = ?, last_name=?, email_id=?, mobile=?, address=? where id = ?";
+		jdbcTemplate.update(sql, client.getFirstName(), client.getLastName(),
+				client.getEmail(), client.getMobile(), client.getAddress(),
+				client.getId());
+		return client;
 	}
 
 }
