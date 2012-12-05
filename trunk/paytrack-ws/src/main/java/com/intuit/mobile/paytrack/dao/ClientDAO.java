3446ac35727f8e382e7839444d67f33464d96c2a
vijayan.srinivasan@gmail.com <vijayan.srinivasan@gmail.com@e6bd0a06-6a4f-6c25-1cdf-f6960705fdf0>
2012-12-05 08:40:55 +0000

    Initial import.

--- trunk/paytrack-ws/src/main/java/com/intuit/mobile/paytrack/dao/ClientDAO.java
+++ trunk/paytrack-ws/src/main/java/com/intuit/mobile/paytrack/dao/ClientDAO.java
@@ -1,15 +1,32 @@
 package com.intuit.mobile.paytrack.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
 
+import com.intuit.mobile.paytrack.dbo.ClientDO;
 import com.intuit.mobile.paytrack.jaxb.Client;
 import com.intuit.mobile.paytrack.jaxb.Clients;
 
 @Repository
-public class ClientDAO {
+public class ClientDAO extends BaseDAO {
 
 	public Clients selectAll(Long providerId) {
-		return null;
+		List<ClientDO> clientDOs = jdbcTemplate
+				.query("select * from clients where provider_id = ?",
+						new BeanPropertyRowMapper<ClientDO>(ClientDO.class),
+						providerId);
+
+		Clients clients = new Clients();
+		List<Client> clientList = clients.getClients();
+		
+		for(ClientDO clientDO: clientDOs){
+			Client client = mapper.map(clientDO, Client.class);
+			clientList.add(client);
+		}
+		return clients;
 	}
 
 	public Client save(Long providerId, Client client) {
