1116367e864c7a65a887d60037a600b27914492b
vijayan.srinivasan@gmail.com <vijayan.srinivasan@gmail.com@e6bd0a06-6a4f-6c25-1cdf-f6960705fdf0>
2012-12-05 10:24:53 +0000

    sample data added

--- paytrack-ws/src/main/java/com/intuit/mobile/paytrack/dao/SqlConstants.java
+++ paytrack-ws/src/main/java/com/intuit/mobile/paytrack/dao/SqlConstants.java
@@ -18,7 +18,7 @@ public interface SqlConstants {
 	String INSERT_PROVIDER_CLIENTS = "INSERT INTO provider_clients (provider_id, client_id "
 			+ " ) " + " VALUES (?,?)";
 
-	String INSERT_RECIEPT = "INSERT INTO reciepts (provider_clients_id,from,to,amount,payment_date) values(?,?,?,?,?)";
+	String INSERT_RECIEPT = "INSERT INTO reciepts (provider_clients_id,`from`,`to`,amount,payment_date) values(?,?,?,?,?)";
 	String SELECT_PROVIDER_CLIENTS = "SELECT * FROM provider_clients WHERE provider_id= ? AND client_id= ?";
 
 	String SELECT_ALL_CLIENTS_FOR_PROVIDER = "select c.* from clients c join provider_clients cp on c.id = cp.client_id where cp.provider_id = ?";
