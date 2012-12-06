5fe850374276c3dcebcc83c37b073c6dddb0e586
vijayan.srinivasan@gmail.com <vijayan.srinivasan@gmail.com@e6bd0a06-6a4f-6c25-1cdf-f6960705fdf0>
2012-12-06 05:47:27 +0000

    - Code for mobile UI added

--- paytrack-ws/src/main/java/com/intuit/mobile/paytrack/controller/ApplicationController.java
+++ paytrack-ws/src/main/java/com/intuit/mobile/paytrack/controller/ApplicationController.java
@@ -7,27 +7,32 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.CleanupFailureDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.intuit.mobile.paytrack.dao.ClientDAO;
+import com.intuit.mobile.paytrack.dao.ProviderDAO;
 import com.intuit.mobile.paytrack.dao.ReceiptDAO;
 import com.intuit.mobile.paytrack.jaxb.Client;
 import com.intuit.mobile.paytrack.jaxb.Clients;
+import com.intuit.mobile.paytrack.jaxb.Provider;
 import com.intuit.mobile.paytrack.jaxb.Providers;
 import com.intuit.mobile.paytrack.jaxb.Receipt;
 import com.intuit.mobile.paytrack.jaxb.Receipts;
 
 @Controller
-public class ReceiptController {
+public class ApplicationController {
 
+	
 	@Autowired
-	private ReceiptDAO receiptDAO;
+	private ProviderDAO providerDAO;
 
 	@Autowired
 	private ClientDAO clientDAO;
+	
+	@Autowired
+	private ReceiptDAO receiptDAO;
 
 	@RequestMapping("receipts.do")
 	public ModelAndView getReceipts(String emailId, HttpServletRequest request) {
@@ -63,5 +68,45 @@ public class ReceiptController {
 
 		return modelAndView;
 
+	}
+		
+	@RequestMapping("save-provider.do")
+	public ModelAndView saveProvider(Provider provider) {
+		provider = providerDAO.save(provider);
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("provider", provider);
+		return modelAndView;
+	}
+	
+	@RequestMapping("show-provider.do")
+	public ModelAndView showProvider() {
+		ModelAndView modelAndView = new ModelAndView("add-provider");
+		return modelAndView;
+	}
+	
+	@RequestMapping("show-client.do")
+	public ModelAndView showClient() {
+		ModelAndView modelAndView = new ModelAndView("add-client");
+		return modelAndView;
+	}
+	
+	@RequestMapping("save-client.do")
+	public ModelAndView saveClient(Long providerId, Client client) {
+		clientDAO.save(providerId, client);
+		ModelAndView modelAndView = new ModelAndView("home");
+		return modelAndView;
+	}
+	
+	@RequestMapping("show-receipt.do")
+	public ModelAndView showReceipt() {
+		ModelAndView modelAndView = new ModelAndView("add-receipt");
+		return modelAndView;
+	}
+	
+	@RequestMapping("save-receipt.do")
+	public ModelAndView saveReceipt(Long providerId, Long clientId, Receipt receipt) {
+		receiptDAO.save(providerId, clientId, receipt);
+		ModelAndView modelAndView = new ModelAndView("home");
+		return modelAndView;
 	}
 }
