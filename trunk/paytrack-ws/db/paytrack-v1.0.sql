e513ed306d7babb3d790b9791e33d0415e7b2747
vijayan.srinivasan@gmail.com <vijayan.srinivasan@gmail.com@e6bd0a06-6a4f-6c25-1cdf-f6960705fdf0>
2012-12-06 14:15:36 +0000

    - db script modified to make all variable lower case

--- trunk/paytrack-ws/db/paytrack-v1.0.sql
+++ trunk/paytrack-ws/db/paytrack-v1.0.sql
@@ -1,57 +1,149 @@
-create database  if not exists `paytrack`;
-use `paytrack`;
+CREATE DATABASE  IF NOT EXISTS `paytrack` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `paytrack`;
+-- MySQL dump 10.13  Distrib 5.1.40, for Win32 (ia32)
+--
+-- Host: 127.0.0.1    Database: paytrack
+-- ------------------------------------------------------
+-- Server version	5.0.18-nt
 
-drop table if exists `receipts`;
-drop table if exists `provider_clients`;
-drop table if exists `clients`;
-drop table if exists `providers`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table `providers` (
-  `id` int(11) not null auto_increment,
-  `first_name` varchar(100)  not null,
-  `last_name` varchar(100)  not null,
-  `email_id` varchar(50)  not null,
-  `mobile_number` varchar(20)  not null,
-  `address` varchar(300)  not null,
-  `service_name` varchar(200)  not null,
-  primary key  (`id`),
-  unique key `uk_email_id` (`email_id`),
-  unique key `uk_mobile` (`mobile_number`)
-) engine=innodb default charset=utf8;
+--
+-- Not dumping tablespaces as no INFORMATION_SCHEMA.FILES table on this server
+--
 
-create table `clients` (
-  `id` int(11) not null auto_increment,
-  `first_name` varchar(100)  not null,
-  `last_name` varchar(100)  not null,
-  `email_id` varchar(50)  not null,
-  `mobile_number` varchar(20)  not null,
-  `address` varchar(300)  not null,
-  primary key  (`id`),
-  unique key `uk_clients_email_id` (`email_id`),
-  unique key `uk_clients_mobile_number` (`mobile_number`)
-) engine=innodb default charset=utf8;
+--
+-- Table structure for table `clients`
+--
 
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL auto_increment,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email_id` varchar(50) NOT NULL,
+  `mobile_number` varchar(20) NOT NULL,
+  `address` varchar(300) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `uk_clients_email_id` (`email_id`),
+  UNIQUE KEY `uk_clients_mobile_number` (`mobile_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table `provider_clients` (
-  `id` int(11) not null auto_increment,
-  `provider_id` int(11) default null,
-  `client_id` int(11) default null,
-  primary key  (`id`),
-  key `provider_foreign_key` (`provider_id`),
-  key `client_foreign_key` (`client_id`),
-  constraint `client_foreign_key` foreign key (`client_id`) references `clients` (`id`) on delete no action on update no action,
-  constraint `provider_foreign_key` foreign key (`provider_id`) references `providers` (`id`) on delete no action on update no action
-) engine=innodb default charset=utf8;
+--
+-- Dumping data for table `clients`
+--
 
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
 
-create table `receipts` (
-  `id` int(11) not null auto_increment,
-  `provider_clients_id` int(11) default null,
-  `from` timestamp null default null,
-  `to` timestamp null default null,
-  `amount` decimal(10,0) default null,
-  `payment_date` timestamp null default null,
-  primary key  (`id`),
-  key `provider_clients_id` (`provider_clients_id`),
-  constraint `provider_clients_id` foreign key (`provider_clients_id`) references `provider_clients` (`id`) on delete no action on update no action
-) engine=innodb default charset=utf8;
+--
+-- Table structure for table `providers`
+--
+
+DROP TABLE IF EXISTS `providers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `providers` (
+  `id` int(11) NOT NULL auto_increment,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email_id` varchar(50) NOT NULL,
+  `mobile_number` varchar(20) NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `service_name` varchar(200) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `uk_email_id` (`email_id`),
+  UNIQUE KEY `uk_mobile` (`mobile_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `providers`
+--
+
+LOCK TABLES `providers` WRITE;
+/*!40000 ALTER TABLE `providers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `providers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `provider_clients`
+--
+
+DROP TABLE IF EXISTS `provider_clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `provider_clients` (
+  `id` int(11) NOT NULL auto_increment,
+  `provider_id` int(11) default NULL,
+  `client_id` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `provider_foreign_key` (`provider_id`),
+  KEY `client_foreign_key` (`client_id`),
+  CONSTRAINT `client_foreign_key` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `provider_foreign_key` FOREIGN KEY (`provider_id`) REFERENCES `providers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `provider_clients`
+--
+
+LOCK TABLES `provider_clients` WRITE;
+/*!40000 ALTER TABLE `provider_clients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `provider_clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `receipts`
+--
+
+DROP TABLE IF EXISTS `receipts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `receipts` (
+  `id` int(11) NOT NULL auto_increment,
+  `provider_clients_id` int(11) default NULL,
+  `from` timestamp NULL default NULL,
+  `to` timestamp NULL default NULL,
+  `amount` decimal(10,0) default NULL,
+  `payment_date` timestamp NULL default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `provider_clients_id` (`provider_clients_id`),
+  CONSTRAINT `provider_clients_id` FOREIGN KEY (`provider_clients_id`) REFERENCES `provider_clients` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receipts`
+--
+
+LOCK TABLES `receipts` WRITE;
+/*!40000 ALTER TABLE `receipts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `receipts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-12-06 19:44:57
